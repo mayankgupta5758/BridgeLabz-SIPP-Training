fb480ab331749dfebc4cba3596809e93d63d95b8
Mayank Gupta <mayankgupta5758@gmail.com>
2025-08-28 09:02:42 +0530

    Add File

---
+++ Java_Regex/Basic_Regex_Problems/LicensePlateValidator.java
@@ -0,0 +1,24 @@
+package Java_Regex.Basic_Regex_Problems;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class LicensePlateValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter License Plate Number : ");
+        String plate = sc.nextLine();
+
+        String regex = "^[A-Z]{2}\\d{4}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        if (pattern.matcher(plate).matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+
+        sc.close();
+    }
+}
